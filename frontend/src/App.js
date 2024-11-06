9d92dad4031fb9e227c8990b060e4507dfdfc5c2
BrunoSantosFF <brunosantosfernandes0@gmail.com>
2024-11-06 14:52:56 -0300

    adicionando front

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import styles from './styles/styles.css'
+import styled from 'styled-components'
+import {toast, ToastContainer} from 'react-toastify'
+import 'react-toastify/ReactToastify.css'
+
+
+
+function App() {
+  return (
+    <div>
+      <div>
+        div test
+      </div>
+      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT}/>
+      bruno
+    </div>    
+  );
+}
+
+export default App;
--- readme.md
+++ readme.md
@@ -1,2 +1,5 @@
 npm init -y
 npm add express nodemon mysql cors
+
+npx create-react-app ./
+npm add styled-components axios react-icons react-toastify
